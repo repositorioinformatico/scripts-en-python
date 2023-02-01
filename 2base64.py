1fc15225d0e0ade88328dcfdcf6f28a0ca44a51f
repositorioinformatico <jdgarcia@iesperemaria.com>
2023-02-01 21:06:19 +0100

    Actualizado script número 2 con el trabajo de clase: - Añadido menú - Añadida opción de base32

--- 2base64.py
+++ 2base64.py
@@ -1,6 +1,31 @@
-#codificar en base64
-import base64
-valorin=input("Inserta un texto para cifrar: ")
-valor=str.encode(valorin)
-codificadoEnBase64=base64.b64encode(valor)
-print(codificadoEnBase64.decode("utf-8"))
+import base64 
+import sys 
+import os 
+salir=False 
+opcion=0 
+def codifica32_64(tipo): 
+  valorin=input("Inserta un texto para cifrar: ") 
+  valor=str.encode(valorin) 
+  if tipo == 32: 
+   codificadoEnBase64=base64.b32encode(valor) 
+  else: 
+   codificadoEnBase64=base64.b64encode(valor) 
+  print(codificadoEnBase64.decode("utf-8")) 
+while not salir: 
+os.system('clear') 
+print("1. Opción 1") 
+print("2. Opción 2") 
+print("3. Salir") 
+opcion=int(input("Elige una opción")) 
+if opcion == 1: 
+  print("Has seleccionado opción 1") 
+  codifica32_64(32) 
+  input("Pulse para continuar al menú") 
+elif opcion == 2: 
+  print("Has seleccionado opción 2") 
+  codifica32_64(64) 
+  input("Pulse para continuar al menú") 
+elif opcion == 3: 
+  exit(127) 
+else: 
+  input("Opción incorrecta.") 
