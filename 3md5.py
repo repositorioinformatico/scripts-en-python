1cc2936663b857900ae5cf3221ee6284fa746e81
repositorioinformatico <jdgarcia@iesperemaria.com>
2023-02-08 16:47:29 +0100

    Añadida autoría de línea 5

--- 3md5.py
+++ 3md5.py
@@ -2,7 +2,7 @@
 import hashlib
 import random
 for x in range(1,100000):
- i = random.randrange(1,1000)
+ i = random.randrange(1,1000) #Ruben's idea
  password = str(i)
  md5 = hashlib.md5(password.encode())
  print("El hash para el numero " + str(i) + " es " + md5.hexdigest())
